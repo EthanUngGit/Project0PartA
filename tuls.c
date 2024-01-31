4b6bcb65792529fa07e9e3457a53dfa5e51117b2
EthanUngGit <ethan.ung@temple.edu>
2024-01-31 11:42:19 -0500

    modified tuls.c

--- tuls.c
+++ tuls.c
@@ -9,14 +9,15 @@ void listFiles(const char *path, int indent) {
     struct dirent *entry;
     DIR *dir = opendir(path);
 
-    // if directory error then exit
+    // if directory null then exit
     if (dir == NULL) {
         printf("tuls: cannot open directory\n");
         exit(1);
     }
 
+    // iterates through each entry in a directory until entry null
     while ((entry = readdir(dir)) != NULL) {
-        // ignore current directory (.) and parent directory (..)
+        // ignore current directory (.) and parent directory (..) to avoid infinite recursion
         if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
             // indent the output based on the level of recursion
             for (int i = 0; i < indent; i++) {
@@ -26,18 +27,18 @@ void listFiles(const char *path, int indent) {
             printf("->%s\n", entry->d_name);
 
             // create the new path by concatenating the current path and the entry name
-            char newPath[1024];
-            snprintf(newPath, sizeof(newPath), "%s/%s", path, entry->d_name);
+            // stores result in newPath array
+            char pathArray[1000];
+            snprintf(pathArray, sizeof(pathArray), "%s/%s", path, entry->d_name);
 
             struct stat statBuf;
-            // get information about the file or directory using stat()
-            if (stat(newPath, &statBuf) == 0 && S_ISDIR(statBuf.st_mode)) {
-                // if the entry is a directory, recursively call listFiles() on it
-                listFiles(newPath, indent + 2);
+            // uses stat() to get information about file or directory
+            if (stat(pathArray, &statBuf) == 0 && S_ISDIR(statBuf.st_mode)) {
+                // if the entry is a directory then call listFiles
+                listFiles(pathArray, indent + 2);
             }
         }
     }
-
     closedir(dir);
 }
 
@@ -48,11 +49,6 @@ int main(int argc, char *argv[]) {
     } else if (argc == 2) {
         // if a directory path is provided as a command line argument, list files in that directory
         listFiles(argv[1], 0);
-    } else {
-        // display the correct usage of the program if the number of arguments is incorrect
-        printf("Usage: ./tuls [directory]\n");
-        exit(1);
     }
-
     return 0;
 }
